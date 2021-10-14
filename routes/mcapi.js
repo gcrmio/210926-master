09c837c24b893300d6dc77fc5b684a2fce006e20
wjchoi <wonjeung.choi@amorepacific.com>
2021-10-14 09:19:59 +0900

    V16_edited mcapi.js - getContent

--- routes/mcapi.js
+++ routes/mcapi.js
@@ -236,13 +236,14 @@ function loadContentList(atoken) {
         //console.log("ContentOptions: ");	
         console.log(error,response.body);
         var tmp = JSON.parse(response.body);
-
+        console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+        console.log(tmp);
+        console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+        
         console.log("");
         console.log("Content List ==============================================================================================");
         for(const item of tmp.items){
             console.log("ID= "+item.id+" Name= "+item.name+" assetType= "+item.assetType.name+" category= "+item.category.id);
-            console.log("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
-            console.log(item.Content);
         }
         console.log("===========================================================================================================");
 
