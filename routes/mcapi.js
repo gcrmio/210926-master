b0ffc4b82785083975e5cc80cfc0f8f7f3f0609b
wjchoi <wonjeung.choi@amorepacific.com>
2021-10-14 14:01:28 +0900

    V17_edited mcapi.js - loadContentBinary

--- routes/mcapi.js
+++ routes/mcapi.js
@@ -355,7 +355,7 @@ function loadContentBinary(atoken) {
         var tmp = JSON.parse(response.body);
 
         console.log("Content Binary Info ==============================================================================================");
-        console.log(tmp);
+        console.log(tmp.id);
         console.log("");
         // console.log(tmp.name);
         // console.log(tmp.fileProperties.publishedURL);
