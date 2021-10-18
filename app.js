da37aeea856cd4369be4017dfbc2c0b6eda9a22e
wjchoi <wonjeung.choi@amorepacific.com>
2021-10-18 13:40:40 +0900

    V24_edited app.js - removed pathname

--- app.js
+++ app.js
@@ -83,9 +83,9 @@ app.use('/upload', express.static('upload'));
 
 app.post('/upload', upload.single('file'), function(req, res, next){
     console.log(JSON.stringify(req.file));
-    res.send(req.file, pathname);
-    console.log('===== REQFILE ' + req.file);
-    console.log('===== PATHNAME ' + pathname);
+    res.send(req.file);
+    // console.log('===== REQFILE ' + req.file);
+    // console.log('===== PATHNAME ' + pathname);
 });
 
 
