47a7074db8bdb34e4a006f4897c8695d44c60d42
wjchoi <wonjeung.choi@amorepacific.com>
2021-10-18 17:22:40 +0900

    V25_edited index.html - button for content builder

--- app.js
+++ app.js
@@ -65,8 +65,6 @@ app.post('/journeybuilder/execute/', activity.execute );
 //});
 
 // added 2021-10-18 for file upload from local pc
-
-
 var storage = multer.diskStorage({
     destination: function(req, file, cb){
         cb(null, 'upload/');
