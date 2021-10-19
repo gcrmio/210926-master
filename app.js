5bca54ffc7fae98e9536857403bac8b013ffd232
wjchoi <wonjeung.choi@amorepacific.com>
2021-10-19 19:51:45 +0900

    V28_edited app.js - upload to S3

--- app.js
+++ app.js
@@ -112,7 +112,7 @@ const upload = multer({
   app.post('/upload', upload.single('file'), function(req, res, next){
       console.log(JSON.stringify(req.file));
       res.send(req.file);
-      res.redirect('/index.html');
+      res.redirect('public/index.html');
       // console.log('===== REQFILE ' + req.file);
       // console.log('===== PATHNAME ' + pathname);
   });
